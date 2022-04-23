13845c5d6245b721f29c949c525b1b22232c44ec
rushali9876 <patilrushali2002@gmail.com>
2022-04-23 01:17:53 -0700

    Added : Addition of Program

---
+++ ADD.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a=5,b=10,c=0,d=0,e=0,f=0;
+	clrscr();
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	printf("\n addition=%f",c);
+	printf("\n subtraction=%f",d);
+	printf("\n multification=%f",e);
+	printf("\n division=%f",f);
+	getch();
+}
