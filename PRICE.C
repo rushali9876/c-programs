b3d7082c5108f25cd39eb47033af10fafcd15beb
rushali <patilrushali2002@gmail.com>
2022-04-25 01:25:51 -0700

    ADDED : [rushali] price

---
+++ PRICE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter The 2 Numbers");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\ price=%d",c);
+	getch();
+}
